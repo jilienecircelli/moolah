13460cb575e9b736b54fee50b2dbcf575d9b0293
TMonies <15614796+TaylorNBurrows@users.noreply.github.com>
2020-01-14 20:54:45 -0800

    updating package.json

--- public/js/budget.js
+++ public/js/budget.js
@@ -48,59 +48,61 @@ $(document).ready(function() {
             $articleTitle.addClass("list-group");
             $(".financeNews-col").append($articleTitle);
 ​
-            jQuery(function($) {
-                $('.table').footable();
-            });
-            // Editor for Foo Table
-            var $modal = $('#editor-modal'),
-                $editor = $('#editor'),
-                $editorTitle = $('#editor-title'),
-                ft = FooTable.init('#editing-example', {
-                    editing: {
-                        enabled: true,
-                        addRow: function() {
-                            $modal.removeData('row');
-                            $editor[0].reset();
-                            $editorTitle.text('Add a new row');
-                            $modal.modal('show');
-                        },
-                        editRow: function(row) {
-                            var values = row.val();
-                            $editor.find('#category').val(values.category);
-                            $editor.find('#amount').val(values.amount);
-                            $editor.find('#totalSpent').val(values.totalSpent);
-​
-                            $modal.data('row', row);
-                            $editorTitle.text('Edit row #' + values.id);
-                            $modal.modal('show');
-                        },
-                        deleteRow: function(row) {
-                            if (confirm('Are you sure you want to delete the row?')) {
-                                row.delete();
-                            }
-                        }
-                    }
-                }),
-                uid = 10;
-​
-            $editor.on('submit', function(e) {
-                if (this.checkValidity && !this.checkValidity()) return;
-                e.preventDefault();
-                var row = $modal.data('row'),
-                    values = {
-                        category: $editor.find('#category').val(),
-                        amount: $editor.find('#amount').val(),
-                        totalSpent: $editor.find('#totalSpent').val(),
-                    };
-​
-                if (row instanceof FooTable.Row) {
-                    row.val(values);
-                } else {
-                    values.id = uid++;
-                    ft.rows.add(values);
-                }
-                $modal.modal('hide');
-            });
+//             jQuery(function($) {
+//                 $('.table').footable();
+//             });
+//             // Editor for Foo Table
+//             var $modal = $('#editor-modal'),
+//                 $editor = $('#editor'),
+//                 $editorTitle = $('#editor-title'),
+//                 ft = FooTable.init('#editing-example', {
+//                     editing: {
+//                         enabled: true,
+//                         addRow: function() {
+//                             $modal.removeData('row');
+//                             $editor[0].reset();
+//                             $editorTitle.text('Add a new row');
+//                             $modal.modal('show');
+//                         },
+//                         editRow: function(row) {
+//                             var values = row.val();
+//                             $editor.find('#category').val(values.category);
+//                             $editor.find('#amount').val(values.amount);
+//                             $editor.find('#totalSpent').val(values.totalSpent);
+// ​
+//                             $modal.data('row', row);
+//                             $editorTitle.text('Edit row #' + values.id);
+//                             $modal.modal('show');
+//                         },
+//                         deleteRow: function(row) {
+//                             if (confirm('Are you sure you want to delete the row?')) {
+//                                 row.delete();
+//                             }
+//                         }
+//                     }
+//                 }),
+//                 uid = 10;
+// ​
+//             $editor.on('submit', function(e) {
+//                 if (this.checkValidity && !this.checkValidity()) return;
+//                 e.preventDefault();
+//                 var row = $modal.data('row'),
+//                     values = {
+//                         category: $editor.find('#category').val(),
+//                         amount: $editor.find('#amount').val(),
+//                         totalSpent: $editor.find('#totalSpent').val(),
+//                     };
+// ​
+//                 if (row instanceof FooTable.Row) {
+//                     row.val(values);
+//                 } else {
+//                     values.id = uid++;
+//                     ft.rows.add(values);
+//                 }
+//                 $modal.modal('hide');
+//             });
+
+            
             var $articleDescription = $(`<h3> Description: ${description}</h3>`);
             $articleDescription.addClass("list-group");
             $(".financeNews-col").append($articleDescription);
