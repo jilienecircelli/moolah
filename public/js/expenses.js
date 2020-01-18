5ca27082da30980d66e6bfe7d59814bc6d82b5d8
TMonies <15614796+TaylorNBurrows@users.noreply.github.com>
2020-01-18 12:27:39 -0800

    save expense button js and post method

--- public/js/expenses.js
+++ public/js/expenses.js
@@ -1,38 +1,53 @@
-var saveExpenseBtn = $("save-expense");
-var expenseNameInput = $("expense-name");
-var expenseAmountInput = $("expense-amount");
-var expenseMonthSelect = $("expense-month");
-var expenseCategorySelect = $("expense-category");
-var userIdPassport = //figure out how to get user id
+var userIdPassport = 1;
+var saveExpenseBtn = $("#save-expense");
+var expenseNameInput = $("#expense-name");
+var expenseAmountInput = $("#expense-amount");
+var expenseMonthSelect = $("#expense-month");
+var expenseCategorySelect = $("#expense-category");
+var editExpenseBtn = $("#table-edit");
+var deleteExpenseBtn = $("#table-delete");
+var expenseTableElement = $("#table-body")
 
-    saveExpenseBtn.on("click", function handleSubmit(e) {
-        e.preventDefault();
 
-        var newExpense = {
-            category: expenseCategorySelect.val(),
-            expenseName: expenseNameInput.val(),
-            amount: expenseAmountInput.val(),
-            month: expenseMonthSelect.val(),
-            userId: userIdPassport,
-        };
+getExpenses();
 
-        console.log(newExpense);
 
-    });
+
+$(saveExpenseBtn).on("click", function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(expenseNameInput.val());
+    console.log(expenseAmountInput.val());
+    console.log(expenseMonthSelect.val());
+    console.log(expenseCategorySelect.val());
+
+
+    var newExpense = {
+        category: expenseCategorySelect.val(),
+        expenseName: expenseNameInput.val(),
+        amount: expenseAmountInput.val(),
+        month: expenseMonthSelect.val(),
+        userID: userIdPassport,
+    };
+
+    console.log(newExpense);
+
+    submitExpense(newExpense);
+});
+
 
 
 function submitExpense(expense) {
-    $.post("/api/expenses/", expense, function () {
-        renderExpenses();
-    });
+    $.ajax({
+      method: "POST",
+      url: "/api/expenses/",
+      data: expense
+    })
+      .then(function() {
+        console.log("this is the expense I am posting" + expense)
+  });
 }
 
-
-function renderExpenses() {
-    //render expense to table
-}
-
-// for creating table elements: 
 {/* <td class="pt-3-half" id="expense-category">
 </td>
     <td class="pt-3-half" id="expense-name">
@@ -47,3 +62,60 @@ function renderExpenses() {
             class="btn btn-secondary btn-rounded btn-sm my-0"><i
                 class="fas fa-trash"></i></button></span>
     </td> */}
+
+function createExpenseRow(expense) {
+    console.log(expense);
+    var newTr = $("<tr>");
+    newTr.data("expense", expense);
+    newTr.append("<td id='expense-category'>" + expense.category + "</td>");
+    newTr.append("<td># of posts will display when we learn joins in the next activity!</td>");
+    newTr.append("<td><a href='/blog?author_id=" + authorData.id + "'>Go to Posts</a></td>");
+    newTr.append("<td><a href='/cms?author_id=" + authorData.id + "'>Create a Post</a></td>");
+    newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
+    return newTr;
+  }
+
+
+
+
+
+
+
+function updateExpense(expense) {
+    $.ajax({
+        method: "PUT",
+        url: "/api/expenses",
+        data: expense
+    })
+        .then(function () {
+            renderExpenses()
+        });
+}
+
+
+function getExpenseData(id) {
+    $.get("/api/posts/" + id, function (data) {
+        if (data) {
+            expenseNameInput.val(data.expenseName);
+            bodyInput.val(data.body);
+            postCategorySelect.val(data.category);
+        }
+    });
+}
+
+function deleteExpenseData(id) {
+    $.ajax({
+        method: "DELETE",
+        url: "/api/expenses/" + id
+    })
+        .then(function () {
+            //what do we do with the data? 
+        });
+}
+
+
+function renderExpenses(expense) {
+    console.log(expense);
+}
+
+// for creating table elements: 
--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 use moolah_db;
 
-INSERT INTO users (firstName, lastName, email, password,createdAt,updatedAt) values ('Taylor', 'Burrows','taylorburrows92@gmail.com','password', '2020-11-01','2020-11-01'), ('Jill', 'Circelli','jilienecircelli@gmail.com','password','2020-11-01','2020-11-01'), ('Laura', 'Hawkins','lhawk1000@gmail.com','password','2020-11-01','2020-11-01');
-INSERT INTO budgets (category,amount,month, userID,createdAt,updatedAt) values ('Housing', 2000, 'January', 1, '2020-11-01','2020-11-01'), ('Utilities', 350, 'January', 1, '2020-11-01','2020-11-01'), ('Food', 800, 'January', 1 ,'2020-11-01','2020-11-01');
-INSERT INTO goals (category, goalName, monthlyContribution, description, amount, userID, createdAt, updatedAt) values ('Transportation', 'New Car', 300,'Contribute 300 a month to buy a new car', 10000, 1, '2020-11-01','2020-11-01'), ('Housing', 'Buy a House', 500, 'Contribute 500 a month to buy a new house', 50000, 1, '2020-11-01','2020-11-01');
-INSERT INTO expenses (category,expenseName,amount, month, userID, createdAt,updatedAt) values ('Housing','Rent',1800, 'January', 1, '2020-11-01','2020-11-01'), ('Utilities','Internet', 120, 'January', 1, '2020-11-01','2020-11-01'), ('Food','Vons', 225, 'January', 1,'2020-11-01','2020-11-01');
+INSERT INTO Users (firstName, lastName, email, password,createdAt,updatedAt) values ('Taylor', 'Burrows','taylorburrows92@gmail.com','password', '2020-11-01','2020-11-01'), ('Jill', 'Circelli','jilienecircelli@gmail.com','password','2020-11-01','2020-11-01'), ('Laura', 'Hawkins','lhawk1000@gmail.com','password','2020-11-01','2020-11-01');
+INSERT INTO Budgets (category,amount,month, userID,createdAt,updatedAt) values ('Housing', 2000, 'January', 1, '2020-11-01','2020-11-01'), ('Utilities', 350, 'January', 1, '2020-11-01','2020-11-01'), ('Food', 800, 'January', 1 ,'2020-11-01','2020-11-01');
+INSERT INTO Goals (category, goalName, monthlyContribution, description, amount, userID, createdAt, updatedAt) values ('Transportation', 'New Car', 300,'Contribute 300 a month to buy a new car', 10000, 1, '2020-11-01','2020-11-01'), ('Housing', 'Buy a House', 500, 'Contribute 500 a month to buy a new house', 50000, 1, '2020-11-01','2020-11-01');
+INSERT INTO Expenses (category,expenseName,amount, month, userID, createdAt,updatedAt) values ('Housing','Rent',1800, 'January', 1, '2020-11-01','2020-11-01'), ('Utilities','Internet', 120, 'January', 1, '2020-11-01','2020-11-01'), ('Food','Vons', 225, 'January', 1,'2020-11-01','2020-11-01');
 
 
