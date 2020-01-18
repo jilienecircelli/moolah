70a325c5cd068368fe174175e63dd5b11ea45db6
lahawkin <lhawk1000@gmail.com>
2020-01-18 11:04:55 -0800

    january jquery variables

--- public/js/budget.js
+++ public/js/budget.js
@@ -17,84 +17,19 @@ $(document).ready(function () {
     // Update Budget Table //
     const $addBudgetBtn = $("#submitBudget");
     const $categoryChoice = $("#categoryChoice");
-    const $budgetAmount = $("#budgetAmount")
+    const $budgetAmount = $("#budgetAmount");
+
+    var $janHousingBdgt = $("#janHousingBdgt");
+    var $janUtilitiesBdgt = $("#janUtilitiesBdgt");
+    var $janTransportationBdgt = $("#janTransportationBdgt"); 
+    var $janFoodBdgt = $("#janFoodBdgt"); 
+    var $janShoppingBdgt =$("#janShoppingBdgt"); 
+    var $janLeisureBdgt = $("#janLeisureBdgt"); 
+    var $janMiscBdgt = $("#janMiscBdgt");
+    var $janTotalSpent = $("#janTotalSpent"); 
+    
+
 
     $addBudgetBtn.on("click", function () {
 
     })
-
-
-
-    // Budget Table
-
-    const $tableID = $('#table');
-    const $BTN = $('#export-btn');
-    const $EXPORT = $('#export');
-
-    const newTr = `
-<tr class="hide">
-  <td class="pt-3-half" contenteditable="true">Example</td>
-  <td class="pt-3-half" contenteditable="true">Example</td>
-  <td class="pt-3-half" contenteditable="true">Example</td>
-  <td class="pt-3-half" contenteditable="true">Example</td>
-  <td class="pt-3-half" contenteditable="true">Example</td>
-  <td class="pt-3-half">
-    <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i></a></span>
-    <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down" aria-hidden="true"></i></a></span>
-  </td>
-  <td>
-    <span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0 waves-effect waves-light">Remove</button></span>
-  </td>
-</tr>`;
-    // Sort feature - if we use it...
-    $tableID.on('click', '.table-up', function () {
-
-        const $row = $(this).parents('tr');
-
-        if ($row.index() === 1) {
-            return;
-        }
-
-        $row.prev().before($row.get(0));
-    });
-
-    $tableID.on('click', '.table-down', function () {
-
-        const $row = $(this).parents('tr');
-        $row.next().after($row.get(0));
-    });
-
-    // A few jQuery helpers for exporting only
-    jQuery.fn.pop = [].pop;
-    jQuery.fn.shift = [].shift;
-
-    $BTN.on('click', () => {
-
-        const $rows = $tableID.find('tr:not(:hidden)');
-        const headers = [];
-        const data = [];
-
-        // Get the headers (add special header logic here)
-        $($rows.shift()).find('th:not(:empty)').each(function () {
-
-            headers.push($(this).text().toLowerCase());
-        });
-
-        // Turn all existing rows into a loopable array
-        $rows.each(function () {
-            const $td = $(this).find('td');
-            const h = {};
-
-            // Use the headers from earlier to name our hash keys
-            headers.forEach((header, i) => {
-
-                h[header] = $td.eq(i).text();
-            });
-
-            data.push(h);
-        });
-
-        // Output the result
-        $EXPORT.text(JSON.stringify(data));
-
-    });
