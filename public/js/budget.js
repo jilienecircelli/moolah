a70d39ee8d53d93f24adba2e6d2cc5c1702a6b99
lahawkin <lhawk1000@gmail.com>
2020-01-18 11:24:33 -0800

    adding more jquery vars

--- public/js/budget.js
+++ public/js/budget.js
@@ -15,20 +15,61 @@ $(document).ready(function () {
 
     // })
     // Update Budget Table //
+
     const $addBudgetBtn = $("#submitBudget");
     const $categoryChoice = $("#categoryChoice");
     const $budgetAmount = $("#budgetAmount");
 
+    //January 
     var $janHousingBdgt = $("#janHousingBdgt");
     var $janUtilitiesBdgt = $("#janUtilitiesBdgt");
-    var $janTransportationBdgt = $("#janTransportationBdgt"); 
-    var $janFoodBdgt = $("#janFoodBdgt"); 
-    var $janShoppingBdgt =$("#janShoppingBdgt"); 
-    var $janLeisureBdgt = $("#janLeisureBdgt"); 
+    var $janTransportationBdgt = $("#janTransportationBdgt");
+    var $janFoodBdgt = $("#janFoodBdgt");
+    var $janShoppingBdgt = $("#janShoppingBdgt");
+    var $janLeisureBdgt = $("#janLeisureBdgt");
     var $janMiscBdgt = $("#janMiscBdgt");
-    var $janTotalSpent = $("#janTotalSpent"); 
-    
+    var $janTotalSpent = $("#janTotalSpent");
 
+    //February
+    var $febHousingBdgt = $("#febHousingBdgt");
+    var $febUtilitiesBdgt = $("#febUtilitiesBdgt");
+    var $febTransportationBdgt = $("#febTransportationBdgt");
+    var $febFoodBdgt = $("#febFoodBdgt");
+    var $febShoppingBdgt = $("#febShoppingBdgt");
+    var $febLeisureBdgt = $("#febLeisureBdgt");
+    var $febMiscBdgt = $("#febMiscBdgt");
+    var $febTotalSpent = $("#febTotalSpent");
+
+    //March
+    var $marHousingBdgt = $("#marHousingBdgt");
+    var $marUtilitiesBdgt = $("#marUtilitiesBdgt");
+    var $marTransportationBdgt = $("#marTransportationBdgt");
+    var $marFoodBdgt = $("#marFoodBdgt");
+    var $marShoppingBdgt = $("#marShoppingBdgt");
+    var $marLeisureBdgt = $("#marLeisureBdgt");
+    var $marMiscBdgt = $("#marMiscBdgt");
+    var $marTotalSpent = $("#marTotalSpent");
+
+    //April
+    var $aprHousingBdgt = $("#aprHousingBdgt");
+    var $aprUtilitiesBdgt = $("#aprUtilitiesBdgt");
+    var $aprTransportationBdgt = $("#aprTransportationBdgt");
+    var $aprFoodBdgt = $("#aprFoodBdgt");
+    var $aprShoppingBdgt = $("#aprShoppingBdgt");
+    var $aprLeisureBdgt = $("#aprLeisureBdgt");
+    var $aprMiscBdgt = $("#aprMiscBdgt");
+    var $aprTotalSpent = $("#aprTotalSpent");
+
+    //May
+    //April
+    var $mayHousingBdgt = $("#mayHousingBdgt");
+    var $aprUtilitiesBdgt = $("#aprUtilitiesBdgt");
+    var $aprTransportationBdgt = $("#aprTransportationBdgt");
+    var $aprFoodBdgt = $("#aprFoodBdgt");
+    var $aprShoppingBdgt = $("#aprShoppingBdgt");
+    var $aprLeisureBdgt = $("#aprLeisureBdgt");
+    var $aprMiscBdgt = $("#aprMiscBdgt");
+    var $aprTotalSpent = $("#aprTotalSpent");
 
     $addBudgetBtn.on("click", function () {
 
