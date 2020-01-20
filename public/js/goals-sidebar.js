522fef305afafdfa7c03949e665b50d792d69ac7
minkyukr <minkya_u@daum.net>
2020-01-19 19:12:24 -0800

    goals-side in progress

--- public/js/goals-sidebar.js
+++ public/js/goals-sidebar.js
@@ -1,4 +1,85 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//     //get the userID number- come back later because it doesnt have any data.
+//     $.get("/api/goals").then(function(goals) {
+//     userID = goals.userID;
+//     console.log("user id " + userID);
+//     });
+
+//     var goalsContainer = $(".goals-col");
+//     var postCategorySelect = $("#category");
+//     postCategorySelect.on("change",handleCategoryChange);
+//     var goals;
+
+//     //here we are getting the goals from database
+//     function getGoals(category) {
+//         var categoryString = category || "";
+//         if (categoryString) {
+//             categoryString = "/category/" + categoryString;
+//         }
+//         $.get("/api/goals" + categoryString, function(data) {
+//             console.log("Goals", data);
+//             goals = data;
+//             if (!goals || !goals.length) {
+//                 displayEmpty();
+//             }
+//             else {
+//                 initializeRows();
+//             }
+//         });
+//     }
+//     getGoals();
+
+//     function initializeRows() {
+//         goalsContainer.empty();
+//         var goalsToAdd = [];
+//         for (var i = 0; i < goals.length; i++) {
+//             goalsToAdd.push(createNewRow(goals[i]));
+//         }
+//         goalsContainer.append(goalsToAdd);
+//     }
+
+//     function createNewRow(goal) {
+//         var newGoalCard = $("<div>");
+//         newGoalCard.addClass("card");
+//         var newGoalCardHeading = $("<div>");
+//         newGoalCardHeading.addClass("card-header");
+//         var newGoalTitle = $("<h2>");
+//         var newGoalDate = $("<small>");
+//         var newGoalCategory = $("<h5>");
+//         newGoalCategory.text(goal.category);
+//         newGoalCategory.css({
+//         float: "right",
+//         "font-weight": "700",
+//         "margin-top":
+//         "-15px"
+//         });
+//         var newGoalCardBody = $("<div>");
+//         newGoalCardBody.addClass("card-body");
+//         var newGoalBody = $("<p>");
+//         newGoalTitle.text(goal.title + " ");
+//         newGoalBody.text(goal.body);
+//         // var formattedDate = new Date(goal.createdAt);
+//         // formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+//         // newGoalDate.text(formattedDate);
+//         newGoalTitle.append(newGoalDate);
+//         newGoalCardHeading.append(newGoalTitle);
+//         newGoalCardHeading.append(newGoalCategory);
+//         newGoalCardBody.append(newGoalBody);
+//         newGoalCard.append(newGoalCardHeading);
+//         newGoalCard.append(newGoalCardBody);
+//         newGoalCard.data("goal", goal);
+//         return newGoalCard;
+//     }
+
+//     function handleCategoryChange() {
+//         var newGoalCategory = $(this).val();
+//         getGoals(newGoalCategory);
+//     }
+// });
+
+
+
+
     // Getting jQuery references to the post body, title, form, and author select
     var postCategorySelect = $("#goal-category");
     var goalName = $("#goalName");
@@ -6,21 +87,6 @@ $(document).ready(function() {
     var goalDescription = $("#goalDescription");
     var goalAmount = $("#goalAmount");
     var goalSubmitBtn = $("#goal-submit");
-
-    function getGoals(id) {
-        $.get("/api/goals-sidebar/user" + id, function(data) {
-            userGoals = data;
-            console.log(JSON.stringify(data));
-    });
-    }
-
-    //get goals data for the user when loading the page
-    $.get("/api/user_data")
-    .then(function(user) {
-    userID = user.id;
-    console.log("user id " + userID);
-    getGoals(userID);
-    });
 
     function submitGoal(newGoal) {
         $.ajax({
@@ -54,99 +120,3 @@ $(document).ready(function() {
         getGoal(userID);
         // getGoals(userID);
     });
-
-
-
-});
-
-
-
-
-
-
-
-// function handleCategoryChange() {
-//     var newGoalCategory = $(this).val();
-//     getPosts(newGoalCategory);
-// }
-
-// function createNewRow(goal) {
-//     var newGoalCard = $("<div>");
-//     newGoalCard.addClass("card");
-//     var newGoalCardHeading = $("<div>");
-//     newGoalCardHeading.addClass("card-header");
-
-//     // NO BUTTONS IN OUR HTML
-
-//     // var deleteBtn = $("<button>");
-//     // deleteBtn.text("x");
-//     // deleteBtn.addClass("delete btn btn-danger");
-//     // var editBtn = $("<button>");
-//     // editBtn.text("EDIT");
-//     // editBtn.addClass("edit btn btn-default");
-
-//     var newGoalTitle = $("<h2>");
-//     var newGoalDate = $("<small>");
-//     var newGoalCategory = $("<h5>");
-//     newGoalCategory.text(goal.category);
-//     newGoalCategory.css({
-//         float: "right",
-//         "font-weight": "700",
-//         "margin-top":
-//             "-15px"
-//     });
-//     var newGoalCardBody = $("<div>");
-//     newGoalCardBody.addClass("card-body");
-//     var newGoalBody = $("<p>");
-//     newGoalTitle.text(goal.title + " ");
-//     newGoalBody.text(goal.body);
-//     var formattedDate = new Date(goal.createdAt);
-//     formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-//     newGoalDate.text(formattedDate);
-//     newGoalTitle.append(newGoalDate);
-//     newGoalCardHeading.append(deleteBtn);
-//     newGoalCardHeading.append(editBtn);
-//     newGoalCardHeading.append(newGoalTitle);
-//     newGoalCardHeading.append(newGoalCategory);
-//     newGoalCardBody.append(newGoalBody);
-//     newGoalCard.append(newGoalCardHeading);
-//     newGoalCard.append(newGoalCardBody);
-//     newGoalCard.data("Goal", goal);
-//     return newGoalCard;
-// }
-// function initializeRows() {
-//     goalsContainer.empty();
-//     var goalsToAdd = [];
-//     for (var i = 0; i < goals.length; i++) {
-//         goalsToAdd.push(createNewRow(goals[i]));
-//     }
-//     goalsContainer.append(goalsToAdd);
-// }
-// // This function constructs a goals' HTML
-
-// $(document).ready(function () {
-//     // blogContainer holds all of our posts
-//     var goalsContainer = $(".goalz-col");
-//     var goalCategorySelect = $("#category");
-//     goalCategorySelect.on("change", handleCategoryChange);
-//     var goals;
-
-//     // This function grabs posts from the database and updates the view
-//     function getGoals(category) {
-//         var categoryString = category || "";
-//         if (categoryString) {
-//             categoryString = "/category/" + categoryString;
-//         }
-//         $.get("/api/goals" + categoryString, function (data) {
-//             console.log("Goals", data);
-//             goals = data;
-//             if (!goals || !goals.length) {
-//                 displayEmpty();
-//             } else {
-//                 initializeRows();
-//             }
-//         });
-//     }
-//     getGoals();
-
-// });
