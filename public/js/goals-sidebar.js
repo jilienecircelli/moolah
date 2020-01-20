5946721e7cb50e86b549437b45ab4e17731cbf52
minkyukr <minkya_u@daum.net>
2020-01-20 00:06:04 -0800

    goals rendering to the budget html page

--- public/js/goals-sidebar.js
+++ public/js/goals-sidebar.js
@@ -1,8 +1,15 @@
+
+
 // $(document).ready(function() {
-//     //get the userID number- come back later because it doesnt have any data.
-//     $.get("/api/goals").then(function(goals) {
-//     userID = goals.userID;
-//     console.log("user id " + userID);
+//     // get the userID number- come back later because it doesnt have any data.
+//     // $.get("/api/goals").then(function(goals){
+//     //     var userID = goals.id[1];
+//     //     console.log(userID);
+//     // })
+//     $.get("/api/user_data")
+//         .then(function (user) {
+//             var userID = user.id;
+//             getGoals(userID);
 //     });
 
 //     var goalsContainer = $(".goals-col");
@@ -38,37 +45,34 @@
 //         goalsContainer.append(goalsToAdd);
 //     }
 
-//     function createNewRow(goal) {
-//         var newGoalCard = $("<div>");
-//         newGoalCard.addClass("card");
-//         var newGoalCardHeading = $("<div>");
-//         newGoalCardHeading.addClass("card-header");
-//         var newGoalTitle = $("<h2>");
-//         var newGoalDate = $("<small>");
-//         var newGoalCategory = $("<h5>");
-//         newGoalCategory.text(goal.category);
-//         newGoalCategory.css({
-//         float: "right",
-//         "font-weight": "700",
-//         "margin-top":
-//         "-15px"
-//         });
-//         var newGoalCardBody = $("<div>");
-//         newGoalCardBody.addClass("card-body");
-//         var newGoalBody = $("<p>");
-//         newGoalTitle.text(goal.title + " ");
-//         newGoalBody.text(goal.body);
-//         // var formattedDate = new Date(goal.createdAt);
-//         // formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-//         // newGoalDate.text(formattedDate);
-//         newGoalTitle.append(newGoalDate);
-//         newGoalCardHeading.append(newGoalTitle);
-//         newGoalCardHeading.append(newGoalCategory);
-//         newGoalCardBody.append(newGoalBody);
-//         newGoalCard.append(newGoalCardHeading);
-//         newGoalCard.append(newGoalCardBody);
-//         newGoalCard.data("goal", goal);
-//         return newGoalCard;
+//     function createNewRow(goals) {
+//         var newGoalName = goals.goalName;
+//         var newGoalDescription = goals.description;
+//         var newGoalCategory = goals.category;
+//         var newGoalAmount = goals.amount;
+//         var newGoalContribution = goals.monthlyContribution;
+        
+//         console.log(newGoalName);
+
+//         var $newGoalHeading = $(`<h5> ${newGoalName}</h5>`);
+//         $newGoalHeading.addClass("card-header");
+//         $(".goals-col").append($newGoalHeading);
+
+//         var $newGoalDescription = $(`<h9> Description: ${newGoalDescription}</h9> <br>`);
+//         $newGoalDescription.addClass("card-text");
+//         $(".goals-col").append($newGoalDescription);
+
+//         var $newGoalCategory = $(`<h9> Category: ${newGoalCategory}</h9> <br>`);
+//         $newGoalCategory.addClass("card-text");
+//         $(".goals-col").append($newGoalCategory);
+
+//         var $newGoalAmount = $(`<h9> Goal amount: ${newGoalAmount}</h9> <br>`);
+//         $newGoalAmount.addClass("card-text");
+//         $(".goals-col").append($newGoalAmount);
+
+//         var $newGoalContribution = $(`<h9> Monthly Contribution: ${newGoalContribution}</h9> <br>`);
+//         $newGoalContribution.addClass("card-text");
+//         $(".goals-col").append($newGoalContribution);
 //     }
 
 //     function handleCategoryChange() {
@@ -77,46 +81,133 @@
 //     }
 // });
 
+//     // Getting jQuery references to the post body, title, form, and author select
+//     var postCategorySelect = $("#goal-category");
+//     var goalName = $("#goalName");
+//     var goalMonthlyContribution = $("#goalMonthlyContribution");
+//     var goalDescription = $("#goalDescription");
+//     var goalAmount = $("#goalAmount");
+//     // var goalSubmitBtn = $("#goal-submit");
+//     var submitGoalBtn = $("#submit-goal")
+
+//     // Adding an event listener for when the form is submitted
+//     $(submitGoalBtn).on("click", function handleFormSubmit(event) {
+//         event.preventDefault();
+
+//         var newGoal = {
+//             category: postCategorySelect.val(),
+//             goalName: goalName.val(),
+//             monthlyContribution: goalMonthlyContribution.val(),
+//             description: goalDescription.val().trim(),
+//             amount: goalAmount.val().trim(),
+//         };
+//         console.log(newGoal);
+
+//         submitGoal(newGoal);
+//         // getGoals(userID);
+//     });
+
+//     function submitGoal(goal) {
+//         $.ajax({
+//             method: "POST",
+//             url: "/api/goals/",
+//             data: goal
+//         })
+//         .then(function () {
+//             console.log("new goal" + JSON.stringify(goal));
+//         })
+//     }
+    // function submitGoal(Post) {
+    //     $.post("/api/goals/", Post, function () {
+    //         window.location.href = "/budget";
+    //     });
+    // }
 
 
-
-    // Getting jQuery references to the post body, title, form, and author select
     var postCategorySelect = $("#goal-category");
     var goalName = $("#goalName");
     var goalMonthlyContribution = $("#goalMonthlyContribution");
     var goalDescription = $("#goalDescription");
     var goalAmount = $("#goalAmount");
-    var goalSubmitBtn = $("#goal-submit");
+    var submitGoalBtn = $("#submit-goal");
+    var userGoals = [];
 
-    function submitGoal(newGoal) {
+    $.get("/api/user_data")
+        .then(function(user) {
+            var userID = user.id;
+            console.log(userID);
+            getGoals(userID);
+        });
+
+    $(submitGoalBtn).on("click", function handleFormSubmit(event) {
+        event.preventDefault();
+                    
+        $.get("api/user_data")
+            .then(function (user) {
+                var userID = user.id;
+
+                console.log(userID)
+
+                var newGoal = {
+                    category: postCategorySelect.val(),
+                    goalName: goalName.val(),
+                    monthlyContribution: goalMonthlyContribution.val(),
+                    description: goalDescription.val().trim(),
+                    amount: goalAmount.val().trim(),
+                    userID: user.id,
+                };
+                console.log(newGoal);
+                submitGoal(newGoal);
+                renderGoalsRows(newGoal);
+            });
+    });
+
+    function submitGoal(goal) {
         $.ajax({
-                method: "POST",
-                url: "/api/goals/",
-                data: newGoal
-            })
-            .then(function() {
-                console.log("this is the goal i am posting" + JSON.stringify(newGoal))
+            method: "POST",
+            url: "/api/goals/",
+            data: goal
+        })
+            .then(function () {
+                console.log("trying thousand times" + JSON.stringify(goal));
             });
     }
 
+    function getGoals(id) {
+        $.get("/api/goals/" + id, function (data) {
+            userGoals = data;
+            console.log(JSON.stringify(data));
+            renderGoalsRows();
+        })
+    }
 
+    function renderGoalsRows (goals) {
+        console.log("createnewgoalrow" + JSON.stringify(goals));
+        var newGoalName = goals.goalName;
+        var newGoalDescription = goals.description;
+        var newGoalCategory = goals.category;
+        var newGoalAmount = goals.amount;
+        var newGoalContribution = goals.monthlyContribution;
+        
+        console.log(newGoalName);
 
-    // Adding an event listener for when the form is submitted
-    $(goalSubmitBtn).on("submit", function handleFormSubmit(event) {
-        event.preventDefault();
+        var $newGoalHeading = $(`<h5> ${newGoalName}</h5>`);
+        $newGoalHeading.addClass("card-header");
+        $(".goals-col").append($newGoalHeading);
 
-        var newGoal = {
-            category: postCategorySelect.val(),
-            goalName: goalName.val(),
-            monthlyContribution: goalMonthlyContribution.val(),
-            description: goalDescription.val().trim(),
-            amount: goalAmount.val().trim(),
-            userID: user.id,
-        };
-        console.log(newGoal);
-        console.log("user id" + user.id);
+        var $newGoalDescription = $(`<h9> Description: ${newGoalDescription}</h9> <br>`);
+        $newGoalDescription.addClass("card-text");
+        $(".goals-col").append($newGoalDescription);
 
-        submitGoal(newGoal);
-        getGoal(userID);
-        // getGoals(userID);
-    });
+        var $newGoalCategory = $(`<h9> Category: ${newGoalCategory}</h9> <br>`);
+        $newGoalCategory.addClass("card-text");
+        $(".goals-col").append($newGoalCategory);
+
+        var $newGoalAmount = $(`<h9> Goal amount: ${newGoalAmount}</h9> <br>`);
+        $newGoalAmount.addClass("card-text");
+        $(".goals-col").append($newGoalAmount);
+
+        var $newGoalContribution = $(`<h9> Monthly Contribution: ${newGoalContribution}</h9> <br>`);
+        $newGoalContribution.addClass("card-text");
+        $(".goals-col").append($newGoalContribution);
+    }
